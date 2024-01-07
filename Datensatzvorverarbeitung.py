2c24e2aa4c96cc7d5034a69d78cadcb479088875
tkrumrei <tkrumrei@uni-muenster.de>
2024-01-07 22:05:19 +0100

    Testis Datensatz Umbenennung

--- Datensatzvorverarbeitung.py
+++ Datensatzvorverarbeitung.py
@@ -4,41 +4,10 @@ from PIL import Image
 from scipy.ndimage import distance_transform_edt
 import os
 
-# Cellpose #############################################
-'''
+########## Distanztransformation ##########
+# Cellpose
+
 ##### Train dataset #####
-path = "D:/Datasets/Cellpose/train/masks/030_masks.png"
-
-# Bild für Verarbeitung bereit machen
-image = Image.open(path)
-image_array = np.array(image)
-
-# Distanztransformationen erstellen
-unique_values = np.unique(image_array)
-
-# Initialisiere ein leeres Bild für die kombinierte Distanztransformation
-combined_dist_transform = np.zeros_like(image_array, dtype=float)
-
-# Distanztransformation für jeden einzigartigen Pixelwert berechnen und zum kombinierten Bild hinzufügen
-for value in unique_values[1:]:  # Überspringe den Wert 0
-    # Binäres Bild erstellen, in dem nur die aktuelle Pixelwertgruppe 1 ist
-    binary_mask = (image_array == value).astype(np.uint8)
-    
-    # Distanztransformation anwenden und skalieren
-    dist_transform = distance_transform_edt(binary_mask)
-    
-    # Normalisiere die Distanzwerte auf den Bereich [0, 255]
-    normalized_dist_transform = (dist_transform * 255) / dist_transform.max()
-    # (dist_transform - dist_transform.min()) / (dist_transform.max() - dist_transform.min()) * 255
-    
-    # Zum kombinierten Bild hinzufügen
-    combined_dist_transform += dist_transform #normalized_dist_transform
-
-# In Bild umwandeln und Array und Bild abspeichern
-combined_dist_transform_image = Image.fromarray(combined_dist_transform.astype(np.uint8))
-combined_dist_transform_image.save("D:/Datasets/Cellpose/train/DistanceTransform/030_DT.png")
-np.save("D:/Datasets/Cellpose/train/DistanceTransform/030_DT.npy", combined_dist_transform)
-'''
 '''
 folder_path = "D:/Datasets/Cellpose/train/masks/"
 folder_output = "D:/Datasets/Cellpose/train/DistanceTransform/"
@@ -120,9 +89,10 @@ for filename in os.listdir(folder_path):
         np.save(saved_array_path, combined_dist_transform)
 '''
 
-# Testis ########################################################
+# Testis 
 
 ##### masks_single_cell_nuclei dataset #####
+'''
 folder_path = "D:/Datasets/testis_nuclei_segmentations/masks_single_cell_nuclei"
 folder_output = "D:/Datasets/testis_nuclei_segmentations/CellDistanceTransform/"
 
@@ -160,32 +130,10 @@ for filename in os.listdir(folder_path):
         combined_dist_transform_image = Image.fromarray(combined_dist_transform.astype(np.uint8))
         combined_dist_transform_image.save(saved_image_path)
         np.save(saved_array_path, combined_dist_transform)
-
-
-
 '''
-old_path = os.path.join(pathT, old_filename)
-
-image = Image.open(old_path)
-image_array = np.array(image)
-unique_values = np.unique(image)
-
-# Distanztransformation anwenden
-dist_transform = distance_transform_edt(image_array)
-
-normalized_dist_transform = (dist_transform * 255) / dist_transform.max()
-
-# Die Distanztransformation in ein Bild umwandeln
-dist_transform_image = Image.fromarray(normalized_dist_transform.astype(np.uint8))
-
-unique_values_dt = np.unique(dist_transform_image)
-print(unique_values_dt)
-
-# Das Bild der Distanztransformation anzeigen (optional)
-dist_transform_image.show()
+        
+##### masks_tubulus dataset #####
 '''
-
-# masks_tubulus
 folder_path = "D:/Datasets/testis_nuclei_segmentations/masks_tubulus"
 folder_output = "D:/Datasets/testis_nuclei_segmentations/TubulusDistanceTransform/"
 
@@ -224,4 +172,36 @@ for filename in os.listdir(folder_path):
         combined_dist_transform_image = Image.fromarray(combined_dist_transform.astype(np.uint8))
         combined_dist_transform_image.save(saved_image_path)
         np.save(saved_array_path, combined_dist_transform)
+'''
 
+########## Umbenennung Testis Datensätze ##########
+'''
+def rename_files_in_folder(folder_path, type):
+    count = 1 # Zähler
+    num_zeros = 4 # 0en
+
+    for filename in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, filename)):
+
+            num_with_zeros = str(count).zfill(num_zeros) # führende 0en hinzufügen
+            new_filename = f"{num_with_zeros}_{type}.png"
+
+            old_filepath = os.path.join(folder_path, filename)
+            new_filepath = os.path.join(folder_path, new_filename)
+
+            os.rename(old_filepath, new_filepath)
+            count += 1
+
+# Funktionsaufruf
+rename_files_in_folder("D:/Datasets/testis_nuclei_segmentations/original_without_background/", "img")
+'''
+
+########## Zusammenführen der Datensätze ##########
+
+# Img
+##### Cellpose train dataset #####
+
+##### #####
+
+# Distance Transform
+##### Cellpose train dataset #####        
