37eecaaf99e04f3924a1e44c7b57602918205ca6
tkrumrei <tkrumrei@uni-muenster.de>
2024-02-09 10:59:20 +0100

    small changes

--- UNet.py
+++ UNet.py
@@ -76,14 +76,3 @@ class UNet(nn.Module):
             x = self.ups[index+1](concat_skip)
 
         return self.final_conv(x)
-    
-def test():
-    x = torch.randn((3, 1, 161, 161))
-    model = UNet(in_channels=1, out_channels=1)
-    preds = model(x)
-    print(preds.shape)
-    print(x.shape)
-    assert preds.shape == x.shape
-
-if __name__ == "__main__":
-    test()
