59778b738490bae0a3281ec2591415ca5b1259d7
tkrumrei <tkrumrei@uni-muenster.de>
2024-02-21 20:28:42 +0100

    watershed_segmentation ready

--- Segmentation.py
+++ Segmentation.py
@@ -1,25 +1,226 @@
 '''
 Schritte:
 - Seeds finden (lokale Minima und lokale Maxima Hintergrund)
-- Seed based watershed Algorithm aus Paper
+- Seed based watershed Algorithm aus Deep Distance Transform Paper
+Pseudocode:
+Data: D                             // distance transform estimate
+Smin                                // list of filtered local minima
+Smax                                // list of background seeds
+Result: list of masks
+foreach s1 in Smin do
+    foreach s2 in Smin\{s1} do
+        , mask1, mask2 = watershed(-abs(D), Smax, s1, s2)
+    if mask1 in candidates then
+        inc mask count(mask1)
+    else
+        candidates.append(mask1)
+    // repeat for mask 2
+    end
+end
+sortCandidatesByCount(masks)        // highest count first
+    foreach mask in candidates do
+        if (!any(mask \ candidates == 0) then
+    list of masks.append(mask)
+    end
+end
 '''
+import matplotlib.pyplot as plt
 import numpy as np
-from scipy.ndimage import maximum_filter, minimum_filter
-from PIL import Image
+from scipy.ndimage import maximum_filter
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
 
-def find_local_minima(image):
-    local_minima = (image == minimum_filter(image, footprint=np.ones((5, 5))))
-    return np.argwhere(local_minima)
 
-def find_local_maxima(image, neighborhood_size=5):
-    local_maxima = (image == maximum_filter(image, footprint=np.ones((neighborhood_size, neighborhood_size))))
-    return np.argwhere(local_maxima)
 
-test = np.load("D:/Datasets/Cellpose/train/DistanceTransform/000_DT.npy")
+def find_local_minima(dist_transform, size=5):
+    dist_transform = -dist_transform
 
-Smin = find_local_minima(test)
+    max_filter = maximum_filter(dist_transform, size=size)
+    print("maximum_filter berechnet")
 
-img = Image.fromarray(test.astype(np.uint8))
+    local_maxima_mask = (dist_transform == max_filter)
 
-img.show()
+    threshold = 0.0
+    local_maxima_mask &= (dist_transform > threshold)
 
+    coordinates = np.argwhere(local_maxima_mask)
+
+    # only for visualization
+    '''
+    # display results
+    fig, axes = plt.subplots(1, 2, figsize=(8, 3), sharex=True, sharey=True)
+    ax = axes.ravel()
+    ax[0].imshow(dist_transform, cmap=plt.cm.gray)
+    ax[0].axis('off')
+    ax[0].set_title('Original')
+
+    ax[1].imshow(dist_transform, cmap=plt.cm.gray)
+    ax[1].autoscale(False)
+    ax[1].plot(coordinates[:, 1], coordinates[:, 0], 'r.')
+    ax[1].axis('off')
+    ax[1].set_title('Peak local max')
+
+    fig.tight_layout()
+
+    plt.show()
+    '''
+    print("find_local_minima fertig")
+    return coordinates
+
+def find_local_maxima(dist_transform, size=20):
+
+    coordinates = peak_local_max(dist_transform, min_distance=size, threshold_abs=0.0)
+    filtered_coordinates = []
+
+    # exclude coordinates that are too far away from the cells
+    for cor in coordinates:
+        top_left_y = cor[0] - size
+        top_left_x = cor[1] - size
+        bottom_right_y = cor[0] + size
+        bottom_right_x = cor[1] + size
+
+        area = dist_transform[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+
+        equal = np.all(area == area[size,size])
+
+        if not equal:
+            filtered_coordinates.append(cor)
+    
+    filtered_coordinates = np.array(filtered_coordinates)
+
+    # only for visualization
+    '''
+    print("peak_local_max berechnet")
+
+    # display results
+    fig, axes = plt.subplots(1, 3, figsize=(8, 3), sharex=True, sharey=True)
+    ax = axes.ravel()
+    ax[0].imshow(dist_transform, cmap=plt.cm.gray)
+    ax[0].axis('off')
+    ax[0].set_title('Original')
+
+    ax[1].imshow(dist_transform, cmap=plt.cm.gray)
+    ax[1].autoscale(False)
+    ax[1].plot(coordinates[:, 1], coordinates[:, 0], 'r.')
+    ax[1].axis('off')
+    ax[1].set_title('Peak local max')
+
+    ax[2].imshow(dist_transform, cmap=plt.cm.gray)
+    ax[2].autoscale(False)
+    ax[2].plot(filtered_coordinates[:, 1], filtered_coordinates[:, 0], 'r.')
+    ax[2].axis('off')
+    ax[2].set_title('Peak local max filtered')
+
+    fig.tight_layout()
+
+    plt.show()
+    '''
+    print("find_local_maxima fertig")
+    return filtered_coordinates
+
+def segmentation(dist_transform, smax, s1, s2):
+    markers = np.zeros_like(dist_transform, dtype=int)
+    markers[s1[0],s1[1]] = 1  # s1
+    markers[s2[0],s2[1]] = 2  # s2
+    for i, seed in enumerate(smax, start=3):
+        markers[seed[0],seed[1]] = i
+
+    # watershed algorithm
+    labels = watershed(-np.abs(dist_transform), markers)
+
+    mask1 = (labels == 1)
+    mask2 = (labels == 2)
+
+    return(mask1, mask2)
+
+def mask_generation_from_seeds(dist_transform):
+    smin = find_local_minima(dist_transform)
+    print(len(smin))
+    smax = find_local_maxima(dist_transform, size=10)
+    print(len(smax))
+
+    candidates = []
+    mask_count = {}
+
+    # make candidate masks and add them to candidates
+    for s1 in smin:
+        for s2 in smin:
+            if not(np.array_equal(s2,s1)):
+                mask1, mask2 = segmentation(dist_transform, smax, s1, s2)
+                
+
+                for i, candidate in enumerate(candidates):
+                    if np.array_equal(candidate, mask1):
+                        mask_count[i] += 1
+                        break
+                else:
+                    candidates.append(mask1)
+                    mask_count[len(candidates) - 1] = 1
+
+
+                for i, candidate in enumerate(candidates):
+                    if np.array_equal(candidate, mask2):
+                        mask_count[i] += 1
+                        break
+                else:
+                    candidates.append(mask2)
+                    mask_count[len(candidates) - 1] = 1
+
+        print("watershed Segmentierung für s1 ist fertig")
+
+    # sortCandidatesByCount(masks) // highest count first
+    sorted_mask_count = sorted(mask_count.items(), key=lambda item: item[1], reverse=True)
+    sorted_candidates = []
+
+    for i, count in sorted_mask_count:
+        sorted_candidates.append(candidates[i])
+    
+    list_of_masks = []
+
+    # candidates are only addes if they contain pixels not covered by any other accepted mask
+    for candidate in sorted_candidates:
+        is_unique = True 
+        for accepted_mask in list_of_masks:
+            if np.any(candidate & accepted_mask):
+                is_unique = False
+                break
+        if is_unique:
+            list_of_masks.append(candidate)
+    
+    print("Liste von Einzelmasken ist fertig")
+    return list_of_masks
+
+def make_mask(dist_transform):
+    list_of_masks = mask_generation_from_seeds(dist_transform)
+    mask = np.zeros_like(dist_transform)
+
+    # make mask
+    for i, candidate in enumerate(list_of_masks):
+        mask[candidate] = i + 1
+
+    print("Maske ist fertig")
+    # only for visualization
+    
+    # display results
+    fig, axes = plt.subplots(1, 2, figsize=(8, 3), sharex=True, sharey=True)
+    ax = axes.ravel()
+    ax[0].imshow(dist_transform, cmap=plt.cm.gray)
+    ax[0].axis('off')
+    ax[0].set_title('Original')
+
+    ax[1].imshow(mask, cmap=plt.cm.gray)
+    ax[1].axis('off')
+    ax[1].set_title('Peak local max')
+
+    fig.tight_layout()
+
+    plt.show()
+    
+    return mask
+
+
+dist_transform = np.load("C:/Users/Tobias/Desktop/test2/test2/distance_transform/11657-28091999_01_x1=1223_y1=3855_x2=2247_y2=4879_dt.npy")
+dist_transform2 = np.load("D:/Datasets/Testis_Model/Cellpose/Train/distance_transform/0108_dt.npy")
+dist_transform3 = np.load("D:/Datasets/Testis_Model/Cellpose/Train/distance_transform/0340_dt.npy")
+
+make_mask(dist_transform2)
