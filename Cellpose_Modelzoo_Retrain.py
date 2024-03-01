54f0f4a97400e7c59974565b664919ebb49a6e08
tkrumrei <tkrumrei@uni-muenster.de>
2024-03-01 10:16:57 +0100

    Modelzoo results fixed

--- Cellpose_Modelzoo_Retrain.py
+++ Cellpose_Modelzoo_Retrain.py
@@ -24,7 +24,7 @@ def calculate_metrics(true_masks, pred_masks):
 
 ######################
 # cellpose_eval(): Evaluates the Best Cellpose model on both datasets with IoU and Dice Score
-def cellpose_eval(model_path, test_path, test_path_2, diam_labels, model_name):
+def cellpose_eval(model_path, test_path, test_path_2, diam_labels, model_name, model_type):
     print(f"Evaluation von {model_name} startet")
     full_model_path = f"{model_path}models/{model_name}"
     use_GPU = core.use_gpu()
@@ -44,7 +44,7 @@ def cellpose_eval(model_path, test_path, test_path_2, diam_labels, model_name):
     average_iou, precision, n_true_p, n_false_p, n_false_n = calculate_metrics(test_labels, masks)
 
 
-    metrics_path = f"{model_path}models/metrics_Cellpose.npy"
+    metrics_path = f"{model_path}models/{model_type}_metrics_Cellpose.npy"
 
     metrics = np.array([average_iou, precision, n_true_p, n_false_p, n_false_n])
     
@@ -62,7 +62,7 @@ def cellpose_eval(model_path, test_path, test_path_2, diam_labels, model_name):
     average_iou, precision, n_true_p, n_false_p, n_false_n = calculate_metrics(test_labels_2, masks_2)
 
 
-    metrics_2_path = f"{model_path}models/metrics_Testis.npy"
+    metrics_2_path = f"{model_path}models/{model_type}metrics_Testis.npy"
 
     metrics_2 = np.array([average_iou, precision, n_true_p, n_false_p, n_false_n])
     np.save(metrics_2_path, metrics_2)
@@ -94,7 +94,7 @@ def cellpose_train(train_path, test_path, test_path_2, model_type, model_name):
     
     diam_labels = model.diam_labels.copy()
     
-    cellpose_eval(train_path, test_path, test_path_2, diam_labels, model_name)
+    cellpose_eval(train_path, test_path, test_path_2, diam_labels, model_name, model_type)
 
 ######################
 # Function calls
@@ -104,11 +104,11 @@ palma_path = "/scratch/tmp/tkrumrei/Cellpose_Model"
 # Cellpose Dataset
 # cellpose_train("D:/Datasets/Cellpose_Model/Cellpose/Train/", "D:/Datasets/Cellpose_Model/Cellpose/Test_Cellpose/", "D:/Datasets/Cellpose_Model/Cellpose/Test_Testis/", None, "Cellpose_Model")
 # Testis Dataset
-cellpose_train(f"{palma_path}/Testis/Fold_1/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cellpose_Testis_Fold_1_Model")
-cellpose_train(f"{palma_path}/Testis/Fold_2/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cellpose_Testis_Fold_2_Model")
-cellpose_train(f"{palma_path}/Testis/Fold_3/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cellpose_Testis_Fold_3_Model")
-cellpose_train(f"{palma_path}/Testis/Fold_4/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cellpose_Testis_Fold_4_Model")
-cellpose_train(f"{palma_path}/Testis/Fold_5/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cellpose_Testis_Fold_5_Model")
+cellpose_train(f"{palma_path}/Testis/Fold_1/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cyto_Testis_Fold_1_Model")
+cellpose_train(f"{palma_path}/Testis/Fold_2/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cyto_Testis_Fold_2_Model")
+cellpose_train(f"{palma_path}/Testis/Fold_3/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cyto_Testis_Fold_3_Model")
+cellpose_train(f"{palma_path}/Testis/Fold_4/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cyto_Testis_Fold_4_Model")
+cellpose_train(f"{palma_path}/Testis/Fold_5/", f"{palma_path}/Testis/Test_Cellpose/", f"{palma_path}/Testis/Test_Testis/", "cyto", "Cyto_Testis_Fold_5_Model")
 # Mix Dataset
 #cellpose_train("D:/Datasets/Cellpose_Model/Mix/Fold_1/", "D:/Datasets/Cellpose_Model/Mix/Test_Cellpose/", "D:/Datasets/Cellpose_Model/Mix/Test_Testis/", None, "Cellpose_Mix_Fold_1_Model")
 #cellpose_train("D:/Datasets/Cellpose_Model/Mix/Fold_2/", "D:/Datasets/Cellpose_Model/Mix/Test_Cellpose/", "D:/Datasets/Cellpose_Model/Mix/Test_Testis/", None, "Cellpose_Mix_Fold_2_Model")
